cd676457d1f49513f326c716fdca85790bbac681
fayhand <mail@allanlorentzen.dk>
2016-11-07 07:47:55 +0100

    Changed to using jspm as package manager, Babel as a transpiler, Systemjs as a module loader

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,18 @@
-(function(angular) {
-	angular.module("team-viewer", ["ngRoute"])
-		.config(function($routeProvider, $locationProvider) 
-		{
-		  	$routeProvider
-		  	.when("/", 
-		  	{
-		  		controller: "HomeController",
-		  		templateUrl: "views/home.html"
-		  	})
-		  	.when("/teams", 
-		  	{
-		  		controller: "HomeController",
-		  		templateUrl: "views/home.html"
-		  	})  
-		  	.otherwise("/")
-		});
-})(window.angular);
+import angular from "angular";
+
+angular.module("team-viewer", ["ngRoute"])
+	.config(function($routeProvider, $locationProvider) 
+	{
+	  	$routeProvider
+	  	.when("/", 
+	  	{
+	  		controller: "HomeController",
+	  		templateUrl: "views/home.html"
+	  	})
+	  	.when("/teams", 
+	  	{
+	  		controller: "HomeController",
+	  		templateUrl: "views/home.html"
+	  	})  
+	  	.otherwise("/")
+	});
