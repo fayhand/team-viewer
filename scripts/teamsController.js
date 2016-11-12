24bf4d2c38f02c684732900983969376944cb8e7
fayhand <mail@allanlorentzen.dk>
2016-11-12 13:24:03 +0100

    Added apiService with getTeams. Teams view shows the response.

--- scripts/teamsController.js
+++ scripts/teamsController.js
@@ -1,8 +1,12 @@
 import angular from "angular";
 
 import app from "./app.js";
+import apiService from "./services/apiService.js";
 
-app.controller('teamsController', function() 
+app.controller('teamsController', ["$scope", "apiService", function($scope, apiService) 
 	{
-
-	});
+		apiService.getTeams().then(function(teams) {
+			console.log(teams);
+			$scope.teams = teams;
+		});
+	}]);
